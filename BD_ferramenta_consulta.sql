1e9f2d5bb89b67b8ea0e08514f13f985199a2cbc
jocieleciele <jociele_siqueira@estudante.sesisenai.org.br>
2024-10-31 19:37:38 -0300

    Update BD_ferramenta_consulta.sql

--- BD_ferramenta_consulta.sql
+++ BD_ferramenta_consulta.sql
@@ -1,7 +1,7 @@
 create database BD_ferramenta_consulta;
 use BD_ferramenta_consulta;
 
-CREATE TABLE Colaboradores (
+CREATE TABLE Colaborador (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(60) NOT NULL,
     matricula VARCHAR(50) NOT NULL UNIQUE,
@@ -11,22 +11,22 @@ CREATE TABLE Colaboradores (
 use BD_ferramenta_consulta;
 CREATE TABLE Treinamentos (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome_treinamento VARCHAR(100) NOT NULL,
+    nome_treinamentos VARCHAR(100) NOT NULL,
     exigido_para_funcao VARCHAR(100) NOT NULL,
     validade_em_anos INT DEFAULT 0,
     status ENUM('Ativo', 'Inativo') NOT NULL DEFAULT 'Ativo'
 );
 
 use BD_ferramenta_consulta;
-CREATE TABLE Colaboradores_Treinamentos (
+CREATE TABLE Colaborador_Treinamentos (
     id INT PRIMARY KEY AUTO_INCREMENT,
     colaborador_id INT NOT NULL,
-    treinamento_id INT NOT NULL,
+    treinamentos_id INT NOT NULL,
     data_conclusao DATE NOT NULL,
     validade DATE NOT NULL,
     status ENUM('Completo', 'Pendente', 'Vencido') NOT NULL,
     CHECK (status IN ('Completo', 'Pendente', 'Vencido')),
-    FOREIGN KEY (colaborador_id) REFERENCES Colaboradores(id),
-    FOREIGN KEY (treinamento_id) REFERENCES Treinamentos(id)
+    FOREIGN KEY (colaborador_id) REFERENCES Colaborador(id),
+    FOREIGN KEY (treinamentos_id) REFERENCES Treinamentos(id)
 );
 
