687cc762b0af1549bcf96743a8247084b35b1d9f
jocieleciele <jociele_siqueira@estudante.sesisenai.org.br>
2024-10-31 19:39:42 -0300

    Update Insert_consulta.sql

--- Insert_consulta.sql
+++ Insert_consulta.sql
@@ -1,5 +1,5 @@
 use Bd_ferramenta_consulta;
-INSERT INTO Colaboradores (nome, matricula, cargo) VALUES 
+INSERT INTO Colaborador (nome, matricula, cargo) VALUES 
 ('Joao da Silva','01234','Operador de Maquinas'),
 ('Maria Oliveira','01235','Tecnico de Seguranca'),
 ('Carlos Santos','01236','Operador de Tratores'),
@@ -7,7 +7,7 @@ INSERT INTO Colaboradores (nome, matricula, cargo) VALUES
 ('Lucas Almeida','01238','Assistente Administrativo');
 
 use Bd_ferramenta_consulta;
-INSERT INTO Treinamentos (nome_treinamento, exigido_para_funcao, validade_em_anos, status) VALUES
+INSERT INTO Treinamentos (nome_treinamentos, exigido_para_funcao, validade_em_anos, status) VALUES
 ('Seguranca no Trabalho', 'Operador de Maquinas', 2, 'Ativo'),
 ('Primeiros Socorros', 'Tecnico de Seguranca', 1, 'Ativo'),
 ('Manuseio de Tratores', 'Operador de Tratores', 3, 'Ativo'),
@@ -15,7 +15,7 @@ INSERT INTO Treinamentos (nome_treinamento, exigido_para_funcao, validade_em_ano
 ('Treinamento Administrativo', 'Assistente Administrativo', 1, 'Inativo');
 
 use Bd_ferramenta_consulta;
-INSERT INTO Colaboradores_Treinamentos (colaborador_id, treinamento_id, data_conclusao, validade, status) VALUES
+INSERT INTO Colaborador_Treinamentos (colaborador_id, treinamentos_id, data_conclusao, validade, status) VALUES
 (1, 1, '2023-01-15', '2025-01-15', 'Completo'),
 (2, 2, '2022-02-10', '2023-02-10', 'Vencido'),
 (3, 3, '2021-03-20', '2024-03-20', 'Completo'),
